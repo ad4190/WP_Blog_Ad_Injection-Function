18b39925044f00a77452aec06caf61f674e5b510
ad4190 <E0013328@u.nus.edu>
2017-06-25 21:58:46 +0800

    Update functions.php
    edited comments

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php 
-function t4a_generate_google_ad_code() {
+function generate_google_ad_code() {
     if (!isset($GLOBALS['AddCounter'])) {
         $GLOBALS['AddCounter']=1;
     } else {
@@ -12,11 +12,11 @@ function t4a_generate_google_ad_code() {
     return $adcode;
         
 }
-function t4a_insert_ad_in_content($content)
+function insert_ad_in_content($content)
 {
     if ((!is_singular('post')) || (strpos($content, '<p') === FALSE) ) {  return $content; }
 
-    $ChapterCount = 2; // Enter the chapter number, which you'd like an add in front of
+    $ChapterCount = 2; // Enter the paragraph number, which you'd like an ad in front of
 
     $content = explode("<p", $content);
 
@@ -24,7 +24,7 @@ function t4a_insert_ad_in_content($content)
     
     for ($i = 0; $i < count($content); $i++) {
         if ($i == $ChapterCount) {
-            $new_content .= t4a_generate_google_ad_code(); // call function for generating ad code
+            $new_content .= generate_google_ad_code(); // call function for generating ad code
         }
         
         if($i>0) { 
@@ -36,4 +36,4 @@ function t4a_insert_ad_in_content($content)
     
     return $new_content;
 }
-add_filter('the_content', 't4a_insert_ad_in_content');
+add_filter('the_content', 'insert_ad_in_content');
